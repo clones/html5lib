267860d00fe896e666d0c8572c5d7ee007d2dd4c
ryansking <ryansking@acbfec75-9323-0410-a652-858a13e371e0>
2007-05-30 05:02:21 +0000

    changing indentation to 2 spaces for ruby style. (closes #41)
    git-svn-id: http://html5lib.googlecode.com/svn/trunk@637 acbfec75-9323-0410-a652-858a13e371e0

--- ruby/tests/preamble.rb
+++ ruby/tests/preamble.rb
@@ -7,7 +7,7 @@ $:.unshift File.join(File.dirname(File.dirname(__FILE__)),'lib')
 $:.unshift File.dirname(__FILE__)
 
 def html5lib_test_files(subdirectory)
-    Dir[File.join(HTML5LIB_BASE, 'tests', subdirectory, '*.*')]
+  Dir[File.join(HTML5LIB_BASE, 'tests', subdirectory, '*.*')]
 end
 
 begin
