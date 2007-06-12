eb082ae369b1712b3ef06d7f713ece10c972da42
jacques.distler <jacques.distler@acbfec75-9323-0410-a652-858a13e371e0>
2007-06-12 00:53:37 +0000

    Fix chardet tests. Search for testdata either in parent directory or in ruby directory.
    git-svn-id: http://html5lib.googlecode.com/svn/trunk@711 acbfec75-9323-0410-a652-858a13e371e0

--- ruby/tests/preamble.rb
+++ ruby/tests/preamble.rb
@@ -1,13 +1,19 @@
 require 'test/unit'
 
-HTML5LIB_BASE = File.dirname(File.dirname(File.dirname(File.expand_path(__FILE__))))
+HTML5LIB_BASE = File.dirname(File.dirname(File.dirname(File.expand_path(__FILE__)))) 
+
+if File.exists?(File.join(HTML5LIB_BASE, 'testdata'))
+  TESTDATA_DIR = File.join(HTML5LIB_BASE, 'testdata')
+else
+  TESTDATA_DIR = File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'testdata')
+end
 
 $:.unshift File.join(File.dirname(File.dirname(__FILE__)),'lib')
 
 $:.unshift File.dirname(__FILE__)
 
 def html5lib_test_files(subdirectory)
-  Dir[File.join(HTML5LIB_BASE, 'testdata', subdirectory, '*.*')]
+  Dir[File.join(TESTDATA_DIR, subdirectory, '*.*')]
 end
 
 begin
