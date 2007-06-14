bfc3566a52b1a7527077dc9423cd184289bdfb98
sa3ruby <sa3ruby@acbfec75-9323-0410-a652-858a13e371e0>
2007-06-14 00:03:48 +0000

    Inject meta tests
    git-svn-id: http://html5lib.googlecode.com/svn/trunk@732 acbfec75-9323-0410-a652-858a13e371e0

--- ruby/lib/html5lib/filters/inject_meta_charset.rb
+++ ruby/lib/html5lib/filters/inject_meta_charset.rb
@@ -9,23 +9,52 @@ module HTML5lib
       end
 
       def each
-        done = false
+        state = :pre_head
+        meta_found = @encoding.nil?
+        pending = []
+
         __getobj__.each do |token|
           case token[:type]
           when :StartTag
-            if token[:name].downcase == "meta" and token[:data]["charset"]
-              done = true
+            state = :in_head if token[:name].downcase == "head"
+
+          when :EmptyTag
+            if token[:name].downcase == "meta"
+              if token[:data].any? {|name,value| name=='charset'}
+                # replace charset with actual encoding
+                attrs=Hash[*token[:data].flatten]
+                attrs['charset'] = @encoding
+                token[:data] = attrs.to_a.sort
+                meta_found = true
+              end
+
+            elsif token[:name].downcase == "head" and not meta_found
+              # insert meta into empty head
+              yield({:type => :StartTag, :name => "head", :data => {}})
+              yield({:type => :EmptyTag, :name => "meta",
+                     :data => {"charset" => @encoding}})
+              yield({:type => :EndTag, :name => "head"})
+              meta_found = true
+              next
             end
 
           when :EndTag
-            if token[:name].downcase == "head"
+            if token[:name].downcase == "head" and pending.any?
+              # insert meta into head (if necessary) and flush pending queue
+              yield pending.shift
               yield({:type => :EmptyTag, :name => "meta",
-                     :data => {"charset" => @encoding}}) if not done
-              done = true
+                     :data => {"charset" => @encoding}}) if not meta_found
+              yield pending.shift while pending.any?
+              meta_found = true
+              state = :post_head
             end
           end
 
-          yield token
+          if state == :in_head
+            pending << token
+          else
+            yield token
+          end
         end
       end
     end
