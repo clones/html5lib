7947888f30e626890ce0bed2b7535b733f06bc6b
ryansking <ryansking@acbfec75-9323-0410-a652-858a13e371e0>
2007-08-08 01:22:47 +0000

    gems/rubyforge support
    git-svn-id: http://html5lib.googlecode.com/svn/trunk@945 acbfec75-9323-0410-a652-858a13e371e0

--- ruby/Rakefile.rb
+++ ruby/Rakefile.rb
@@ -1,7 +1,18 @@
 require 'rake'
-require 'rake/testtask'
+require 'hoe'
+require 'lib/html5/version'
 
-Rake::TestTask.new do |task|
-  task.pattern = 'tests/test_*.rb'
-  task.verbose = true
+Hoe.new("html5", HTML5::VERSION) do |p|
+  p.name = "html5"
+  p.description = p.paragraphs_of('README', 2..5).join("\n\n")
+  p.summary = "HTML5 parser/tokenizer."
+
+  p.author   = ['Ryan King'] # TODO: add more names
+  p.email    = 'ryan@theryanking.com'
+  p.url      = 'http://code.google.com/p/html5lib'
+  p.need_zip = true
+
+  p.extra_deps << ['chardet', '>= 0.9.0']
+  p.changes = p.paragraphs_of('History.txt', 0..1).join("\n\n")
+  p p.paragraphs_of('History.txt', 0..1).join("\n\n")
 end
